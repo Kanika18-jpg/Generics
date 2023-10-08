7f0a3742edd40154e86db933b28e69f4e0949e18
hp <sabhrwalkanika1999@gmail.com>
2023-10-08 19:17:04 +0530

    Add project files.

---
+++ Generics/genericClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+     class genericClass <T>
+    {
+        T message;
+
+        public genericClass(T message)
+        {
+            this.message = message;
+            Console.WriteLine(message);
+        }
+    }
+    class Program1
+    {
+        public static void Main(string[] args)
+        {
+            genericClass<string> g1 = new genericClass<string>("Hello");
+            genericClass<int> g2 = new genericClass<int>(1);
+            genericClass<char> g3 = new genericClass<char>('K');
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Generics/genericMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class genericMethod
+    {
+        public static void Show<T>(T[] a)
+        {
+            for(int i=0; i<a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            Show(a);
+            string[] b = { "Kanika", "Deevanshu" };
+            Show(b);
+
+            Console.ReadLine();
+        }
+    }
+}
